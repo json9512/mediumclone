9039c8ae38d533217abafd58b3a775d2ea33e979
Son <json9512@gmail.com>
2020-11-28 01:21:44 +0900

    Setup PSQL

---
+++ server/src/models/pools.js
@@ -0,0 +1,6 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+import { PSQL_CONNECTION_STRING } from '../settings';
+dotenv.config();
+
+export const pool = new Pool({connectionString: PSQL_CONNECTION_STRING});
--- server/src/settings.js
+++ server/src/settings.js
@@ -2,3 +2,4 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 export const testEnvVar = process.env.TEST_ENV_VAR
+export const PSQL_CONNECTION_STRING = process.env.PSQL_CONNECTION_STRING
