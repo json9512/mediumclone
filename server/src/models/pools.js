f63b3f535db294d5f1557b070087f1702ebcb6dd
Son <json9512@gmail.com>
2020-11-28 12:41:51 +0900

    Edit pools.js

--- server/src/models/pools.js
+++ server/src/models/pools.js
@@ -3,4 +3,9 @@ import dotenv from 'dotenv';
 import { PSQL_CONNECTION_STRING } from '../settings';
 dotenv.config();
 
-export const pool = new Pool({connectionString: PSQL_CONNECTION_STRING});
+export const pool = new Pool({
+    connectionString: PSQL_CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
