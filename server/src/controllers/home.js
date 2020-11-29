95a8cc3c75c1dab20afebbffd7ef7136dea74d3d
Son <json9512@gmail.com>
2020-11-29 23:58:11 +0900

    Comment JWT func

--- server/src/controllers/home.js
+++ server/src/controllers/home.js
@@ -2,14 +2,14 @@ import {testEnvVar} from '../settings';
 import {generateAccessToken} from '../utils/jwt';
 
 export const indexPage = (req, res) => {
-    /**Create JWT token here if user is logged in*/
+    /**Create JWT token here if user is logged in
     if (req.user){
         console.log("Logged in at index")
         // JSON webtoken comes here
         const token = generateAccessToken({username: req.user._json.nickname});
         console.log(token)
         
-    }
+    }*/
 
 
     return res.render('home', {title: testEnvVar})
